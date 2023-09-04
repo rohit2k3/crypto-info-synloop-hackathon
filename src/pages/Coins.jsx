ce8998b8ebae22e5d0ae77471d38ae31ff03de9f
Rohit <rohitsharma2k3@gmail.com>
2023-09-04 13:25:05 +0530

    auth update

--- src/pages/Coins.jsx
+++ src/pages/Coins.jsx
@@ -29,7 +29,7 @@ const Coins = () => {
           {
             headers: {
               Authorization:
-                "Bearer 6KPnAjasx2BzGyQSF2Lnfw51zqpeNjBy5lHN66E8Fgw5s8dxWsDmpr5DQQAOP7uBKhwOeYrBljpGnJKrpaUWEQ6h9JmFScSEHjiYZQ1NZXVIr+OTK6TXB2NWImPfVdzp5zZn3whU5FPFl1xPyCVG8Ug1J7PYhOEIBERuRcusi1gesnoesM5p4u/ZDL9yYQRaUebQo0la7up4inpGRRwa4eQiH4RIFHoleBHsmXp6SoSrrOjIcUpCWjNzg9yufgKnMV2afFwQDgv2pazhrfyTa3HwFXerHrL/9AcV4NXKmGJy+Nu56D3Hg8ufPfjPYsuZG3RRP2Avl36DyAH3sGlirobhrwhcE8yDZ1z6fd1FD/2vsjh/mbvyIcmFiotFmqYWzUU5ub2poFHrFrLgtVOBzEe564lmvaU2XZt9SKhGkH5wc2cM9U1ig9XJRWA8BqKy6/6uIvhYsoGgeWO7VIbOb+23Pd8xV+f7xKfKgpMxu4MTZRtzma1UEAO0QOM5cKHdz4hrqy6ebmKYIh/AtppwBcwy4to9IoGrns36Z9rd+YdpRZo/A8wOyDUAc2y1qXBl",
+                "Bearer 6KPnAjasx2BzGyQSF2Lnfw51zqpeNjBy5lHN66E8Fgw5s8dxWsDmpr5DQQAOP7uBKhwOeYrBljpGnJKrpaUWEQ6h9JmFScSEHjiYZQ1NZXVIr+OTK6TXB2NWImPfVdzp5zZn3whU5FPFl1xPyCVG8Ug1J7PYhOEIBERuRcusi1gesnoesM5p4u/ZDL9yYQRaUebQo0la7up4inpGRRwa4eQiH4RIFHoleBHsmXp6SoRDjEH77UoZYnr5la3UcBWo0GhjbZvf164i+2kZlwysMy6rrtD+PSAZz9tavyKg1vLKgHA5ebgBpgaCh4HRYSHhjdbilDVii1Fk+Ydh5wwHblQ7m82WkZXWiR+ICl16yMbvHMjOW1VPyTNpA+lCWKdyMw1RYzCFSMx26Xm7iG82SCmiYpNVSodgvkvQo8KR3cWkUgh3DnFpk4rG9EYyv9h2i4qhintlNKHKPoRZclHSxcsIx7lulzJ2x19NRnayyiOfAHeF/1Q5VITxNm5owkQp",
             },
           }
         );
